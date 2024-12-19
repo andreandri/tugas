c0ad2008c117f75c6c5a7e8adf70d1b58c14f229
andreandri <165983950+andreandri@users.noreply.github.com>
2024-12-19 21:02:53 +0700

    a

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['id_user'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Kesehatan</title>
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/index.css">
 </head>
 <body>
     <header>
